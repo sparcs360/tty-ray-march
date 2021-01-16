58ba7b273fd9b159f907bd65c7045e187887e95b
Lee Newfeld <lee.newfeld@gmail.com>
2021-01-16 23:13:43 +0000

    Polish

--- src/gl/objects/index.ts
+++ src/gl/objects/index.ts
@@ -1,6 +1,4 @@
-import { SceneObject } from './sceneObject';
-import { HPlane } from './hplane';
-import { Sphere } from './sphere';
-import { Torus } from './torus';
-
-export { SceneObject, HPlane, Sphere, Torus };
+export { SceneObject } from './sceneObject';
+export { HPlane } from './hplane';
+export { Sphere } from './sphere';
+export { Torus } from './torus';
