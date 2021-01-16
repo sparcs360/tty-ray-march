e0550772ca112c0d5098abec2649c238c4282836
Lee Newfeld <lee.newfeld@gmail.com>
2021-01-16 23:03:55 +0000

    Polish

--- src/gl/index.ts
+++ src/gl/index.ts
@@ -1,18 +1,5 @@
-import { Director, SceneUpdateFunction } from './director';
-import { Scene } from './scene';
-import { SceneObject, HPlane, Sphere } from './objects';
-import { Light, DiffusedLight } from './lights';
-import { TtyRenderer, ShaderFunction } from './ttyRenderer';
-
-export {
-  Director,
-  SceneUpdateFunction,
-  Scene,
-  SceneObject,
-  HPlane,
-  Sphere,
-  Light,
-  DiffusedLight,
-  TtyRenderer,
-  ShaderFunction,
-};
+export { Director, SceneUpdateFunction } from './director';
+export { Scene } from './scene';
+export { SceneObject, HPlane, Sphere } from './objects';
+export { Light, DiffusedLight } from './lights';
+export { TtyRenderer, ShaderFunction } from './ttyRenderer';
